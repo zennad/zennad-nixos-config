a98f9ce18d9e1f857ce0def3f06b19dbfd582e39
zennad <zennad@gmail.com>
2020-05-20 12:49:36 +0000

    Commit my config file
    This is a rough config for nixos.
    
    Future work:
    * Look at configuring xmonad from this file
    * Change the display manager? maybe xdm?
    * Change the firewall to have some more sensible options than only
    allowing 443 and 22 TCP and blocking all UDP
    * Get sound to work
    * Try and figure out why zstd compression doesn’t seem to work for the
    zram swap even though the nixos configuration documentation lists it
    as an option.

---
+++ configuration.nix
@@ -0,0 +1,113 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # boot.kernelModules = [ "zcache" ];
+
+  boot.initrd.luks.devices.crypted.device = "/dev/disk/by-uuid/d7589e83-ec1d-4738-823f-929875f1bdb2";
+
+  zramSwap = {
+    enable = true;
+    # algorithm = "zstd";
+  };
+
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    networkmanager.enable = true;
+
+    # Configure network proxy if necessary
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Open ports in the firewall.
+    firewall.allowedTCPPorts = [ 443 22 ];
+    # firewall.allowedUDPPorts = [ ];
+    # Or disable the firewall altogether.
+    # firewall.enable = false;
+  };
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+    curl
+    networkmanager
+    zsh
+    bash
+    gnupg
+    emacs
+    ghc
+    stack
+    xmonad-with-packages
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+
+  services.xserver.windowManager.xmonad = {
+    enable = true;
+    enableContribAndExtras = true;
+  };
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.guest = {
+  #   isNormalUser = true;
+  #   uid = 1000;
+  # };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.09"; # Did you read the comment?
+
+}
