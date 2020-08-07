447cc34e48b69399628ebdb52d8928dc8459d25d
zennad <zennad@gmail.com>
2020-08-07 01:11:38 +0000

    Add hardware-configuration.nix
    Adjusted .gitignore and began tracking hardware-configuration.nix
    
    Despite my reluctance to have my hardware configuration included in
    this repository I have done so in order to experiment with making my
    NixOS config into a flake.
    
    Working with git tracked files is much easier in flakes than simple
    paths, it is also better form.
    
    We’ll see if this shows any use.

--- flake.nix
+++ flake.nix
@@ -1,10 +1,15 @@
 {
   description = "zennad's configuration flake";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
+    hardware-config.url = "/etc/nixos/hardware-configuration.nix";
+    hardware-config.flake = false;
+  };
+  outputs = { self, nixpkgs, hardware-config }: {
     nixosConfigurations."nixos.zennad" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
     };
+    nixosModules = [ hardware-config ];
   };
 }
