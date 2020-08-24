55272f69c2a8a416556027d7a0004d7068d78fdf
zennad <zennad@gmail.com>
2020-08-24 03:19:08 +0100

    Add flake.nix and flake.lock files

--- flake.nix
+++ flake.nix
@@ -2,16 +2,17 @@
   description = "zennad's configuration flake";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
-    hardware-config = {
-      url = "/etc/nixos/hardware-configuration.nix";
-      flake = false;
-    };
+    # hardware-config = {
+    #   url = "/etc/nixos/hardware-configuration.nix";
+    #   flake = false;
+    # };
   };
-  outputs = { self, nixpkgs, hardware-config }: {
-    nixosConfigurations."nixos.zennad" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+  outputs = { self, ... }@deps: {
+    nixosConfigurations."nixos.zennad" =
+      deps.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
     };
-    nixosModules = [ hardware-config ];
+    nixosModules = [ ./hardware-configuration.nix ];
   };
 }
