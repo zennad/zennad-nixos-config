cee4d23a5eae6778259f5b0cf7962cf290d18e7e
zennad <zennad101@gmail.com>
2020-12-13 14:26:45 +0000

    Remove nixos-hardware from input to flake
    Removes unnecessary mention of `nixos-hardware' in flake inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "zennad's configuration flake";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-20.03;
-    nixos-hardware.url = github:NixOS/nixos-hardware/master;
   };
   outputs = { self, ... }:
     {
