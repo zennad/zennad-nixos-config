ae6d92204e67b6ea272dcbe2aad18dad69155c5e
zennad <zennad@gmail.com>
2020-12-13 03:04:51 +0000

    Remove double quotes around uris in ‘flake.nix’ file
    There is no real reason that I’m doing this other than for aesthetic
    reasons.

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "zennad's configuration flake";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-20.03;
+    nixos-hardware.url = github:NixOS/nixos-hardware/master;
   };
   outputs = { self
             , nixpkgs
